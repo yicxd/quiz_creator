7980948ea5dc742c281aa2ac4a66d327261be455
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-12 23:50:59 +0800

    organized and seperated inputs by lines

--- quiz.py
+++ quiz.py
@@ -1,12 +1,12 @@
 question = input("Enter a question: ")
+sep_question = question + "\n" #seperates the questions by lines, for organizing
+question_file = open("questions.txt", "a")#opens file name and a is append
+question_file.write(sep_question)
+question_file.close()
 
 for i in range(4):
     answers = input(f"Enter four answers for that question No {i+1}:")
-
-question_file = open("questions.txt", "a")#opens file name and a is append
-question_file.write(question)
-question_file.close()
-
-answer_file = open("answers.txt", "a")
-answer_file.write(answers)
-answer_file.close()
+    sep_answers = answers + "\n"
+    answer_file = open("answers.txt", "a")
+    answer_file.write(sep_answers)
+    answer_file.close()
