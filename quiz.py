126a6c28282f3542fc0641bf66c962d5a31e4776
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-12 23:03:50 +0800

    now asks 4 answers for the question

--- quiz.py
+++ quiz.py
@@ -1,4 +1,12 @@
 question = input("Enter a question: ")
 
-file = open("questions.txt", "a")#opens file name and a is append
-file.write(question)
+for i in range(4):
+    answers = input(f"Enter four answers for that question No {i+1}:")
+
+question_file = open("questions.txt", "a")#opens file name and a is append
+question_file.write(question)
+question_file.close()
+
+answer_file = open("answers.txt", "a")
+answer_file.write(answers)
+answer_file.close()
