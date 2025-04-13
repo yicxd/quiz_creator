0ad2fc53b8702ca54e649fa3886fe1a083843eb9
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-13 22:37:50 +0800

    added the rows for the inputs and added a correct answer input

--- quiz.py
+++ quiz.py
@@ -5,23 +5,32 @@ app = QApplication([]) #this is the application that will open
 window = QWidget() #this is the receiver of mouse and keyboard actions
 window.setWindowTitle("Quiz Creator") #title
 window.setFixedSize(600, 400) #resolution of the window
+layout = QFormLayout() #Qformlayout uses a form layout
 
 
 while True:
+    window.setLayout(layout)
     window.show() #will open the window
     try:
+        layout.addRow("Question:", QLineEdit())
         question = input("Enter a question: ")
         sep_question = question + "\n" #seperates the questions by lines, for organizing
         question_file = open("questions.txt", "a") #opens file name and a is append
         question_file.write(sep_question) #will write the input in the txt file
         question_file.close()
 
-        for i in range(4):
-            answers = input(f"Enter four answers for that question No {i+1}: ")
+        for i in range(97, 101):
+            layout.addRow("Option " + chr(i) + ") ", QLineEdit())
+            answers = input("Enter four answers for that question - Option " + chr(i) + ") ")#used characters as choices
             sep_answers = answers + "\n"
             answer_file = open("answers.txt", "a")
             answer_file.write(sep_answers)
             answer_file.close()
+
+        correct = None
+        while correct not in {"a", "b", "c", "d", "A", "B", "C", "D"}:
+            correct = input('Which one is the correct answer? a, b, c, d: ')
+            continue
 
         again = input("If you would like to test this code again type '1' and if you want to end it all type anything: ")
         if again == "1":
