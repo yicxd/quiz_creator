6db8ea08f1dc3bcd4fd829c497ab50b943bf7103
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-13 21:53:45 +0800

    will be using PyQt6 as gui

--- quiz.py
+++ quiz.py
@@ -1,3 +1,6 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QFormLayout, QLineEdit, QWidget
+
 while True:
     try:
         question = input("Enter a question: ")
@@ -7,13 +10,13 @@ while True:
         question_file.close()
 
         for i in range(4):
-            answers = input(f"Enter four answers for that question No {i+1}:")
+            answers = input(f"Enter four answers for that question No {i+1}: ")
             sep_answers = answers + "\n"
             answer_file = open("answers.txt", "a")
             answer_file.write(sep_answers)
             answer_file.close()
 
-        again = input("If you would like to test this code again type '1' and if you want to end it all type anything")
+        again = input("If you would like to test this code again type '1' and if you want to end it all type anything: ")
         if again == "1":
             continue
         else:
