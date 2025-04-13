13357d9378a45accedf1a208f4ce650e1a9d1528
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-13 22:07:12 +0800

    created the window that will open up

--- quiz.py
+++ quiz.py
@@ -1,12 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QFormLayout, QLineEdit, QWidget
 
+app = QApplication([]) #this is the application that will open
+window = QWidget() #this is the receiver of mouse and keyboard actions
+window.setWindowTitle("Quiz Creator") #title
+window.setFixedSize(600, 400) #resolution of the window
+
+
 while True:
+    window.show() #will open the window
     try:
         question = input("Enter a question: ")
         sep_question = question + "\n" #seperates the questions by lines, for organizing
-        question_file = open("questions.txt", "a")#opens file name and a is append
-        question_file.write(sep_question)
+        question_file = open("questions.txt", "a") #opens file name and a is append
+        question_file.write(sep_question) #will write the input in the txt file
         question_file.close()
 
         for i in range(4):
@@ -21,6 +28,7 @@ while True:
             continue
         else:
             exit()
+            sys.exit(app.exec()) #the application will also close
 
     except ValueError:
         print("how did you trigger ValueError?")
