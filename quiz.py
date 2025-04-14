5b10033d6f32fe559b261a2d4102d1962f6fcc36
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-14 21:34:18 +0800

    major rework, gui is bugging with while loops so i will now make it as an init function

--- quiz.py
+++ quiz.py
@@ -4,45 +4,34 @@ from PyQt6.QtWidgets import (
     QLabel, QComboBox, QCheckBox, QPushButton
 )
 
-app = QApplication([]) #this is the application that will open
-window = QWidget() #this is the receiver of mouse and keyboard actions
-window.setWindowTitle("Quiz Creator") #title
-window.setFixedSize(500, 600) #resolution of the window
-layout = QFormLayout() #Qformlayout uses a form layout
+class quiz(QWidget):
+    def __init__(self):
+        super().__init__()
 
-#spacing of the layout
-layout.setVerticalSpacing(15)
-layout.setHorizontalSpacing(20) 
-layout.setContentsMargins(30, 30, 30, 30)
+        app = QApplication([]) #this is the application that will open
+        window = QWidget() #this is the receiver of mouse and keyboard actions
+        window.setWindowTitle("Quiz Creator") #title
+        window.setFixedSize(500, 600) #resolution of the window
+        
+        #layout type and spacing
+        layout = QFormLayout()
+        layout.setVerticalSpacing(15)
+        layout.setHorizontalSpacing(20) 
+        layout.setContentsMargins(30, 30, 30, 30)
 
 
-while True:
-    window.setLayout(layout)
-    window.show() #will open the window
-    try:
+        app.exec()
+        window.setLayout(layout)
+        window.show() #will open the window
+
         question = layout.addRow(QLabel("Question:")) #adds a row to accept input
         sep_question = str(question) + "\n" #seperates the questions by lines, for organizing
         question_file = open("questions.txt", "a") #opens file name and a is append
         question_file.write(sep_question) #will write the input in the txt file
         question_file.close()
 
-        for i in range(97, 101): #97 is 'a' in character
-            answers = layout.addRow(QLabel("Enter four answers for that question - Option " + chr(i) + ") "))#switches the number into characters
-            sep_answers = str(answers) + "\n"
-            answer_file = open("answers.txt", "a")
-            answer_file.write(sep_answers)
-            answer_file.close()
-
-        correct = None
-        while correct not in {"a", "b", "c", "d", "A", "B", "C", "D"}:
-            correct = layout.addRow(QLabel("Which one is the correct answer? a, b, c, d: "))
-            continue
-        else:
-            break
-
-#will use a button instead for retries
-
-    except ValueError:
-        print("how did you trigger ValueError?")
-
-sys.exit(app.exec())
+        answers = layout.addRow(QLabel("Enter four answers for that question - Option " + chr(i) + ") "))#switches the number into characters
+        sep_answers = str(answers) + "\n"
+        answer_file = open("answers.txt", "a")
+        answer_file.write(sep_answers)
+        answer_file.close()
