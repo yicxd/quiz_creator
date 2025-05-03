40df34ffb81db79717c32455b932183df0be571d
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-05-03 21:27:16 +0800

    brainstorming, not really a pseudocode

---
+++ the_quiz.py
@@ -0,0 +1,23 @@
+"""
+ok so in the question file each question is seperated by lines
+so we could just print each line for each question
+"""
+
+"""
+in the answer file the odd numbers are the choices
+while the right answers are the even numbered lines
+"""
+
+"""
+for randomization the program will pick a number and then print that line of question
+for the answers its the number of the line question times 2 (for correct answer) minus 1 (the choices)
+
+example
+
+question    choices    answer
+1           1          2
+2           3          4
+3           5          6
+4           7          8
+5           9          10
+"""
