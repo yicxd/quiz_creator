f37fd73d79d556a6bf9b136abada60c9b2ef8549
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-05-09 17:18:11 +0800

    will be using multiple functions + plus some copy and paste from quiz_create

--- the_quiz.py
+++ the_quiz.py
@@ -1,32 +1,39 @@
+import sys
 import tkinter as tk
 from tkinter import ttk, messagebox
 import random
 
-#the window
-window = tk.Tk()
-window.title("Quiz")
-window.geometry("600x600")
-window.mainloop() #opens the window
-        
+class the_quiz():
+    def __init__(self, root):
+        self.window = root
+        self.window.title("Quiz")
+        self.window.geometry("600x600")
 
-#file management
-question = open("questions.txt")
-quest_line = question.readlines()
-total_lines = len(quest_line)
-question.close()
+            
+    def questions():
+        #file management
+        question = open("questions.txt")
+        quest_line = question.readlines()
+        total_lines = len(quest_line)
+        question.close()
 
-answer = open("answers.txt")
-ans_line = answer.readlines()
-question.close()
+        answer = open("answers.txt")
+        ans_line = answer.readlines()
+        question.close()
 
-#randomization
-random_num = random.randrange(0, total_lines)
+        #randomization
+        random_num = random.randrange(0, total_lines)
 
-#print the quiz
-rand_quest = quest_line[random_num]
-choices = ans_line[random_num * 2]
-correct_ans = ans_line[(random_num * 2) + 1]
+        #print the quiz
+        rand_quest = quest_line[random_num]
+        choices = ans_line[random_num * 2]
+        correct_ans = ans_line[(random_num * 2) + 1]
 
-print (rand_quest)
-print (choices)
-print (correct_ans)
+        print (rand_quest)
+        print (choices)
+        print (correct_ans)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = the_quiz(root)
+    root.mainloop()
